7bc1209c97cf10abd95b4f5b851866c165339387
Alejandro Montes <alejandro.montes.g@gmail.com>
2016-02-16 12:07:12 +0100

    Getting user input

--- app.js
+++ app.js
@@ -10,5 +10,18 @@ app.controller('MainCtrl', [
             {title: 'post 4', upvotes: 9},
             {title: 'post 5', upvotes: 4}
         ];
+        $scope.addPost = function() {
+            if(!$scope.title || $scope.title === '') return;
+            $scope.posts.push({
+                title: $scope.title,
+                upvotes: 0,
+                link: $scope.link
+            });
+            $scope.title = '';
+            $scope.link = '';
+        };
+	$scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
     }
 ]);
