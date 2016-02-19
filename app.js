c24cb20d5ccfc58d5cfe6849d3dd7ee48343dcd6
Alejandro Montes <alejandro.montes.g@gmail.com>
2016-02-19 16:48:57 +0100

    The post page

--- app.js
+++ app.js
@@ -17,13 +17,38 @@ app.controller('MainCtrl', [
             $scope.posts.push({
                 title: $scope.title,
                 upvotes: 0,
-                link: $scope.link
+                link: $scope.link,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
             });
             $scope.title = '';
             $scope.link = '';
         };
         $scope.incrementUpvotes = function(post) {
             post.upvotes += 1;
+        };
+    }
+]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+        $scope.addComment = function() {
+            if($scope.body === '') return;
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
+        $scope.incrementUpvotes = function(comment) {
+            comment.upvotes += 1;
         };
     }
 ]);
@@ -36,6 +61,10 @@ app.config([
             url: '/home',
             templateUrl: '/home.html',
             controller: 'MainCtrl'
+        }).state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl',
         });
         $urlRouterProvider.otherwise('home');
     }
